43a7107da6f984b317c63bbfd33d5ad763f7e952
Seaton Jiang <hi@seatonjiang.com>
2025-11-22 23:05:47 +0800

    1

---
+++ kratos-plugin-aws.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Kratos - 对象存储工具包
+ * Description: 使用分布式存储服务所需的工具包，具有高扩展性、低成本、可靠安全等优点。
+ * Version: 2025.11.21
+ * Plugin URI: https://github.com/seatonjiang/kratos-plugin-aws
+ * Author: Seaton Jiang
+ * Author URI: https://seatonjiang.com
+ * License: MIT License
+ * License URI: https://github.com/seatonjiang/kratos-plugin-aws/blob/main/LICENSE
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (!has_action('kratos-plugin-aws')) {
+    add_action('kratos-plugin-aws', function () {
+        require_once plugin_dir_path(__FILE__) . '/vendor/autoload.php';
+    });
+}
